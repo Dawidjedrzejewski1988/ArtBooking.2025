9ad87fe36d2903e76529c4a7187e8671d2516fe8
Dawid Jędrzejewski <dawid.jedrzejewski@microsoft.wsei.edu.pl>
2025-03-28 23:13:33 +0100

    first commit

---
+++ Backend/Controllers/TestController.cs
@@ -0,0 +1,17 @@
+using Microsoft.AspNetCore.Mvc;
+ 
+namespace Backend.Controllers;
+ 
+[ApiController]
+[Route("api/[controller]")]
+public class TestController : ControllerBase
+{
+    [HttpGet()]
+    public ActionResult Check()
+    {
+        return Ok(new
+        {
+            Message = "Check successfull"
+        });
+    }
+}
